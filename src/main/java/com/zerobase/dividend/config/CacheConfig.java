fc091d827c58e383472e41d443502c28b89ddeb0
yu hyun ju <67223214+hyeonju0121@users.noreply.github.com>
2023-08-15 09:23:09 +0900

    Feat : CacheConfig 설정 추가

---
+++ src/main/java/com/zerobase/dividend/config/CacheConfig.java
@@ -0,0 +1,47 @@
+package com.zerobase.dividend.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cache.CacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@RequiredArgsConstructor
+@Configuration
+public class CacheConfig {
+
+    // application.yml 에 레디스 관련 설정 가져오기
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public CacheManager redisCacheManager(RedisConnectionFactory redisConnectionFactory) {
+        RedisCacheConfiguration conf = RedisCacheConfiguration.defaultCacheConfig()
+                // 키와 값을 직렬화
+                .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer()));
+        return RedisCacheManager.RedisCacheManagerBuilder
+                .fromConnectionFactory(redisConnectionFactory)
+                .cacheDefaults(conf)
+                .build();
+    }
+
+    @Bean // 레디스와 커넥션을 맺을 수 있는 커넥션 팩토리 생성
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration conf = new RedisStandaloneConfiguration();
+        conf.setHostName(this.host);
+        conf.setPort(this.port);
+        return new LettuceConnectionFactory(conf);
+    }
+}
